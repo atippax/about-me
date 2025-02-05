e423e4940074094b90bbca4e32e663ab7b113c36
atippax <atip.nasakun.work@gmail.com>
2025-02-05 23:42:38 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   site: "https://atippax.github.io",
-  base: "/portfolio",
+  base: isProduction ? "/portfolio" : "/",
+  prefetch: true,
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
